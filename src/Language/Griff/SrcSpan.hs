25263f77c7fe89299a96a96c5da208dfb27ae6a8
Yuya Kono <takohati0821@gmail.com>
2019-03-25 23:09:51 +0900

    ASTを追加

---
+++ src/Language/Griff/SrcSpan.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE DeriveDataTypeable  #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE InstanceSigs        #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Language.Griff.SrcSpan where
+
+import           Data.Data
+import           Data.Outputable
+import           Data.Text
+import           Data.Text.Prettyprint.Doc
+import           GHC.Generics
+
+type Line = Int
+type Column = Int
+
+data SrcSpan = SrcSpan
+  { srcSpanFilename    :: Text
+  , srcSpanStartLine   :: Line
+  , srcSpanStartColumn :: Column
+  , srcSpanEndLine     :: Line
+  , srcSpanEndColumn   :: Column
+  } deriving (Show, Ord, Generic, Data)
+
+instance Eq SrcSpan where
+  _ == _ = True
+
+instance Outputable SrcSpan
+
+instance Semigroup SrcSpan where
+  (<>) x y = SrcSpan (srcSpanFilename x) (startLine x y) (startColumn x y) (endLine x y) (endColumn x y)
+    where
+      startLine x y = min (srcSpanStartLine x) (srcSpanStartLine y)
+      startColumn x y | srcSpanStartLine x < srcSpanStartLine y = srcSpanStartColumn x
+                      | srcSpanStartLine x == srcSpanStartLine y = min (srcSpanStartColumn x) (srcSpanStartColumn y)
+                      | otherwise = srcSpanStartColumn y
+      endLine x y = max (srcSpanEndLine x) (srcSpanEndLine y)
+      endColumn x y | srcSpanEndLine x < srcSpanEndLine y = srcSpanEndColumn y
+                    | srcSpanEndLine x == srcSpanEndLine y = max (srcSpanEndColumn x) (srcSpanEndColumn y)
+                    | otherwise = srcSpanEndColumn x
+
+
+instance Pretty SrcSpan where
+  pretty (SrcSpan filename startLine startColumn endLine endColumn) =
+    parens $ align $ sep [ pretty filename
+                         , pretty (startLine, startColumn) <> "-" <> pretty (endLine, endColumn) ]
+
+noSrcSpan :: SrcSpan
+noSrcSpan = SrcSpan "no info" 0 0 0 0
---
+++ src/Language/Griff/Syntax.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE EmptyDataDeriving  #-}
+{-# LANGUAGE KindSignatures     #-}
+module Language.Griff.Syntax where
+
+import           Data.Data
+import           Data.Outputable
+import           GHC.Generics
+import           Language.Griff.SrcSpan
+
+data Exp a = Var SrcSpan a
+           | Int SrcSpan Integer
+           | Bool SrcSpan Bool
+           | Char SrcSpan Char
+           | String SrcSpan String
+           | Apply SrcSpan (Exp a) (Exp a)
+           | Lambda SrcSpan (Pat a) (Exp a) (Exp a)
+           | Let SrcSpan (Pat a) (Exp a) (Exp a)
+           | LetRec SrcSpan [(Pat a, Exp a)] (Exp a)
+           | Parens SrcSpan (Exp a)
+           | Case SrcSpan (Exp a) [(Pat a, Exp a)]
+  deriving (Eq, Ord, Show, Generic, Data)
+
+instance Outputable a => Outputable (Exp a)
+
+data Pat a = VarP SrcSpan a
+           | IntP SrcSpan Integer
+           | BoolP SrcSpan Bool
+           | CharP SrcSpan Char
+           | StringP SrcSpan String
+           | ConstructorP SrcSpan a [Pat a]
+  deriving (Eq, Ord, Show, Generic, Data)
+
+instance Outputable a => Outputable (Pat a)
+
+data Dec a = ScAnn SrcSpan a (Type a)
+           | ScDef SrcSpan a (Exp a)
+           | TypeDef SrcSpan a [a] [(a, Type a)]
+  deriving (Eq, Ord, Show, Generic, Data)
+
+instance Outputable a => Outputable (Dec a)
+
+data Type a = TyApp SrcSpan (Type a) (Type a)
+            | TyCon SrcSpan a
+            | TyVar SrcSpan a
+  deriving (Eq, Ord, Show, Generic, Data)
+
+instance Outputable a => Outputable (Type a)
