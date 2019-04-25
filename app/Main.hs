97e82790a4ac885089e86a80c3dd397bc1a4f1f3
Yuya Kono <takohati0821@gmail.com>
2019-04-25 18:52:44 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
+import Language.Griff.Instr
 import Language.Griff.Vm
 
+consTest :: [Instr]
+consTest =
+  [ Block 0 0
+  , Ldi 42
+  , Block 1 2
+  , Invoke 0 [ Ldi 0 ]
+  , Invoke 1 [ Ldi 1 ]
+  , Ldi 2
+  ]
+
 main :: IO ()
-main = print =<< test
+main = print =<< eval consTest [] [] []
