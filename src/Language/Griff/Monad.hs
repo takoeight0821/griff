9d38d4dc286c5047c317577200023f7f12b69111
Yuya Kono <takohati0821@gmail.com>
2019-07-01 18:11:07 +0900

    Renameを実装

--- src/Language/Griff/Monad.hs
+++ src/Language/Griff/Monad.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE DeriveFunctor              #-}
@@ -15,20 +17,16 @@ import           Data.Text
 import           GHC.Generics
 
 data Ctx = Ctx { _uniq :: IORef Int
-               -- , _consTable :: IORef [(Id, (Int, Int))]
                , _fileName :: Text
                } deriving Generic
 
 newtype GriffT m a = GriffT (ReaderT Ctx m a)
   deriving (Functor, Applicative, Monad)
   deriving (HasState "uniq" Int) via ReaderIORef (Rename "_uniq" (Field "_uniq" () (MonadReader (ReaderT Ctx m))))
-  -- deriving (HasState "consTable" [(Id, (Int, Int))]) via ReaderIORef (Rename "_consTable" (Field "_consTable" () (MonadReader (ReaderT Ctx m))))
   deriving (HasReader "fileName" Text) via Rename "_fileName" (Field "_fileName" () (MonadReader (ReaderT Ctx m)))
 
 runGriffT :: MonadIO m => Text -> GriffT m a -> m a
 runGriffT fileName (GriffT m) = do
   i <- liftIO $ newIORef 0
-  -- c <- liftIO $ newIORef []
   runReaderT m $ Ctx { _uniq = i
-                     -- , _consTable = c
                      , _fileName = fileName }
