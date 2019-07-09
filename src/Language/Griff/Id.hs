738b8c4d1477b983ab63f3424415919506f2ca8f
Yuya Kono <takohati0821@gmail.com>
2019-07-09 11:32:43 +0900

    Language.Griff.Uniqを追加

--- src/Language/Griff/Id.hs
+++ src/Language/Griff/Id.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DeriveGeneric #-}
-module Language.Griff.Id (Id(..), stringify) where
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE FlexibleContexts   #-}
+module Language.Griff.Id (Id(..), stringify, newId) where
 
 import           Data.Data
 import           Data.String
-import           Data.Text    (Text, unpack)
+import           Data.Text           (Text, pack, unpack)
 import           GHC.Generics
+import           Language.Griff.Uniq
 
 newtype Id = Id Text
   deriving (Show, Eq, Ord, Generic, Data)
 
 stringify :: IsString a => Id -> a
 stringify (Id text) = fromString $ unpack text
+
+newId :: HasUniq m => Text -> m Id
+newId name = Id . (name<>) . pack . show <$> newUniq
---
+++ src/Language/Griff/Uniq.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE ConstraintKinds  #-}
+{-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
+module Language.Griff.Uniq where
+
+import           Capability.State
+
+type HasUniq m = HasState "uniq" Int m
+
+newUniq :: HasUniq m => m Int
+newUniq = do
+  i <- get @"uniq"
+  modify @"uniq" (+1)
+  return i
