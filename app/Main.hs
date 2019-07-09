0a22536f1e3ae5abca68ce899b9f84059a2eafdd
Yuya Kono <takohati0821@gmail.com>
2019-07-09 12:04:15 +0900

    定数のコンパイルを実装

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,16 @@
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main = putStrLn "hello, world"
+import Language.Griff.IR.LIR
+import Language.Griff.Reducer.CodeGen
+import qualified Data.ByteString.Char8 as BS
+import LLVM.Module
+import LLVM.Context
+
+main :: IO ()
+main = do
+  let mod =  codeGen "hello" (Int 42)
+  withContext $ \ctx -> do
+    llvm <- withModuleFromAST ctx mod moduleLLVMAssembly
+    BS.putStrLn llvm
