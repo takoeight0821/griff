100a674a7a5149afffa6cc48ef59fea0633ff5e5
Yuya Kono <takohati0821@gmail.com>
2019-10-24 23:12:04 +0900

    Language.Griff.Preludeを追加

---
+++ src/Language/Griff/Prelude.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Language.Griff.Prelude
+  ( module Prelude
+  , module Data.Bifunctor
+  , module Data.Bitraversable 
+  , firstM
+  , secondM
+  ) where
+
+import           Data.Bifunctor
+import           Data.Bitraversable
+import           Prelude
+
+firstM :: (Applicative f, Bitraversable t) => (a -> f c) -> t a b -> f (t c b)
+firstM f = bitraverse f pure
+
+secondM :: (Applicative f, Bitraversable t) => (b -> f d) -> t a b -> f (t a d)
+secondM = bitraverse pure
