42ec733fc63c896e599548cf6e42c14b29ac8bb5
Yuya Kono <takohati0821@gmail.com>
2019-11-05 18:28:26 +0900

    Language.Griff.ToFlatを作成

---
+++ src/Language/Griff/ToFlat.hs
@@ -0,0 +1,11 @@
+module Language.Griff.ToFlat (convert) where
+
+import           Control.Effect
+import           Control.Effect.State
+import           Language.Griff.Core     as C
+import           Language.Griff.FlatCore as F
+import           Language.Griff.Id
+
+
+convert :: Toplevel -> [ScDef]
+convert (Toplevel scdefs env conMap) = undefined
