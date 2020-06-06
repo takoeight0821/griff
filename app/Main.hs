abe770ee4c04456c83c2b5abf254de199a5f6fb1
Yuya Kono <takohati0821@gmail.com>
2020-06-06 18:00:29 +0900

    reset project

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,6 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 module Main where
 
-import           Language.Griff.Driver
-import           Language.Griff.Prelude
-import           System.Environment
-import           System.Exit
-
-usage :: IO ()
-usage = do
-  putStr "usage: "
-  exe <- getProgName
-  putStr exe
-  putStrLn " <file name>"
+import Lib
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [filePath] -> compileFromPath filePath
-    _          -> usage >> exitFailure
+main = someFunc
